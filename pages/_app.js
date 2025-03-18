b377c3c70e9b9e22dafe2ce1e63f98f3079dbb57
Nikolas <nik.steppenwolf@web.de>
2025-03-18 14:57:12 +0100

    intermed push

--- pages/_app.js
+++ pages/_app.js
@@ -1,54 +1,48 @@
 import Header from "@/components/Header";
 import Navigation from "@/components/Navigation";
-import { useState } from "react";
-import useSWR from 'swr';
+import { useState, useEffect } from "react";
+import useSWR from "swr";
 import GlobalStyle from "../styles";
 
+const URL = "https://example-apis.vercel.app/api/art";
+
+const fetcher = async (url) => {
+  const res = await fetch(url);
+  if (!res.ok) {
+    const error = new Error("An error occurred while fetching the data.");
+    error.info = await res.json();
+    error.status = res.status;
+    throw error;
+  }
+
+  return res.json();
+};
+
 export default function App({ Component, pageProps }) {
-
-
-
-  const URL = "https://example-apis.vercel.app/api/art"
-
-  const fetcher = async url => {
-    const res = await fetch(url)
-
-    // If the status code is not in the range 200-299,
-    // we still try to parse and throw it.
-    if (!res.ok) {
-      const error = new Error('An error occurred while fetching the data.')
-      // Attach extra info to the error object.
-      error.info = await res.json()
-      error.status = res.status
-      throw error
-    }
-
-    return res.json()
-  }
-
-
-  function Arts() {
-    const { data: arts, error, isLoading } = useSWR(URL, fetcher)
-
-    if (error) return <div>failed to load</div>
-    if (isLoading) return <div>loading...</div>
-
-    return arts
-  }
-
   const [arts, setArts] = useState([]);
 
-  console.log(arts);
+  const { data: fetchedArts, error, isLoading } = useSWR(URL, fetcher);
+  useEffect(() => {
+    if (!fetchedArts) return;
+    setArts([...fetchedArts]);
+  }, [fetchedArts]);
+  if (error) {
+    return <div>failed to load</div>;
+  }
+  if (isLoading) {
+    return <div>loading...</div>;
+  }
 
+  console.log(arts);
   return (
     <>
       <GlobalStyle />
       <Header />
 
       <Component {...pageProps} />
-      {arts.map((art) => (
+      {/* {arts.map((art) => (
         <h2>{art.name}</h2>
-      ))}
+      ))} */}
 
       <Navigation />
     </>
