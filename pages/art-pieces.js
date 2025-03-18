bafe634642028523b24e38644bd27bedf368fd56
Bjoern <beck.b.hh@gmail.com>
2025-03-18 16:59:29 +0100

    Bugfix Card Remove unused Cards component from ArtPieces page

--- pages/art-pieces.js
+++ pages/art-pieces.js
@@ -1,13 +1,10 @@
-import Cards from "@/components/Cards";
 import CardsList from "@/components/CardsList";
 
 export default function ArtPieces({ arts }) {
   return (
     <>
       <h1>Overview Page Art-Pieces</h1>
-      <Cards>hallo</Cards>
       <CardsList arts={arts} />
-
     </>
   );
 }
