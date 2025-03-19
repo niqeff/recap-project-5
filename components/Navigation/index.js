1449dff26ffe355b12a7743ce75d4163f4cfa85e
Nikolas <nik.steppenwolf@web.de>
2025-03-19 10:21:11 +0100

    solved

--- components/Navigation/index.js
+++ components/Navigation/index.js
@@ -1,16 +1,26 @@
 import Link from "next/link";
 import styles from "./Navigation.module.css";
 
-
 export default function Navigation() {
-    return (
-        <nav className={styles.navigation}>
-            <ul className={styles.navigation__list}>
-                <li className={styles.navigation__list}><Link href="./" className={styles.navigation__link}>Spotlight</Link></li>
-                <li className={styles.navigation__list}><Link href="/art-pieces" className={styles.navigation__link}>Art Pieces</Link></li>
-                <li className={styles.navigation__list}><Link href="/favourites" className={styles.navigation__link}>Favourites</Link></li>
-            </ul>
-        </nav>
-    );
+  return (
+    <nav className={styles.navigation}>
+      <ul className={styles.navigation__list}>
+        <li className={styles.navigation__list}>
+          <Link href="./" className={styles.navigation__link}>
+            Spotlight
+          </Link>
+        </li>
+        <li className={styles.navigation__list}>
+          <Link href="/art-pieces" className={styles.navigation__link}>
+            Gallery
+          </Link>
+        </li>
+        <li className={styles.navigation__list}>
+          <Link href="/favourites" className={styles.navigation__link}>
+            Favourites
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
-
