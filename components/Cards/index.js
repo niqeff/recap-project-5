fdfa847f6abda7779ed577bfe40a279149416247
Bjoern <beck.b.hh@gmail.com>
2025-03-19 10:05:13 +0100

    Refactor Spotlight component to use Cards component for rendering artwork; replace styled components with reusable Cards

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -1,3 +1,4 @@
+import Image from "next/image"
 import styles from "./Cards.module.css"
 
 export default function Cards({ artist, title, imgSrc }) {
@@ -5,9 +6,10 @@ export default function Cards({ artist, title, imgSrc }) {
         <>
             <div className={styles['card-container']}>
                 <div className={styles.card}>
-                    <img src={imgSrc} alt="" />
-                    <p>Title:{title}</p>
-                    <p>Artist:{artist}</p>
+                    <Image src={imgSrc} alt={title} width={500} height={500} layout="responsive" />
+                    {title ? <p>Title: {title}</p> : null}
+                    {/* title &&  <p>Title: {title}</p> */}
+                    <p>Artist: {artist}</p>
                 </div>
             </div>
         </>
--- components/Spotlight/index.js
+++ components/Spotlight/index.js
@@ -1,22 +1,8 @@
-import styled from "styled-components";
-import Image from "next/image";
+import Cards from "../Cards";
 
-const StyledCard = styled.div`
-  width: min(100% - 32px, 352px);
-  margin: 0 auto;
-  padding: 32px 0;
-`;
-
-const StyledImage = styled(Image)`
-  object-fit: cover;
-  user-select: none;
-`;
 
 export default function Spotlight({ art }) {
   return (
-    <StyledCard>
-      <StyledImage src={art.imageSource} fill alt="" />
-      <span>{art.artist}</span>
-    </StyledCard>
+    <Cards imgSrc={art.imageSource} artist={art.artist} />
   );
 }
